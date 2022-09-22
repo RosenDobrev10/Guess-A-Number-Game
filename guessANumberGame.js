3869aa02be9b26e49ee7f938a8fc3f259176bd8b
Rosen Dobrev <104829819+RosenDobrev10@users.noreply.github.com>
2022-09-22 13:46:16 +0300

    Update guessANumberGame.js

--- guessANumberGame.js
+++ guessANumberGame.js
@@ -6,26 +6,33 @@ function guessANumber() {
         output: process.stdout,
     });
 
-    let computerGuess = Math.floor(Math.random() * 100);
-    let guess;
+    let computerNumber = Math.floor(Math.random() * 100);
+    let userInput;
+    let userTries = 0
 
     let recursiveAsyncReadLine = function () {
-        readline.question("Guess the number (0-100): ", (number) => {
-            guess = Number(number);
+        readline.question("Guess a number between 0 and 100. You have only 10 tries: ", (input) => {
+            userInput = Number(input);
+            userTries++
 
-            if (guess >= 0 && guess <= 100) {
-                if (guess === computerGuess) {
-                    console.log("You guess it!");
+            if (userTries > 10) {
+                console.log(`Sorry! You have lost. The number is ${computerNumber}!`);
+                return readline.close();
+            }
+
+            if (userInput >= 0 && userInput <= 100) {
+                if (userInput === computerNumber) {
+                    console.log(`Congratulations! You have won. The number is ${computerNumber}!`);
                     return readline.close();
-                } else if (guess < computerGuess) {
-                    console.log("Too Low!");
+                } else if (userInput < computerNumber) {
+                    console.log("Try higher!");
                     recursiveAsyncReadLine();
-                } else if (guess > computerGuess) {
-                    console.log("Too High!");
+                } else if (userInput > computerNumber) {
+                    console.log("Try lower!");
                     recursiveAsyncReadLine();
                 }
             } else {
-                console.log("Invalid input! Try again...");
+                console.log("Invalid input! Please enter a number");
                 recursiveAsyncReadLine();
             }
         });
